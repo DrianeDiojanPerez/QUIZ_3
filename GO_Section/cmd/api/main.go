831706751f4d8d765e323fce83bd28d286e68b15
Driane Diojan Perez <drianeperez12@gmail.com>
2022-10-22 22:54:29 -0600

    added models to main

--- GO_Section/cmd/api/main.go
+++ GO_Section/cmd/api/main.go
@@ -12,41 +12,46 @@ import (
 	"time"
 
 	_ "github.com/lib/pq"
+	"quiz.3.driane.perez.net/internal/data"
 )
 
 // The Application version number
 const version = "1.0.0"
+
 // The Configuration setting
 
 type config struct {
 	port int
-	env string // Development , staging, Production, etc.
-	db struct {
+	env  string // Development , staging, Production, etc.
+	db   struct {
 		//are gotten by flags
-		dsn string
+		dsn               string
 		maxOpenConnection int
 		maxIdleConnection int
-		maxIdleTime string
+		maxIdleTime       string
 	}
 }
-//Dependency Injection
+
+// Dependency Injection
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
+
 func main() {
 
 	var cfg config
 	//read in the flags that are needed to populate our config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env","development", "Environment (development | stagging | production )")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development | stagging | production )")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("TODO_LIST_DB_DSN"), "PostgresSQL DSN")
-	flag.IntVar(&cfg.db.maxOpenConnection,"db-max-open-connection",25,"PostgreSQL max open connection")
-	flag.IntVar(&cfg.db.maxIdleConnection,"db-max-idle-connection",25,"PostgreSQL max idle connection")
-	flag.StringVar(&cfg.db.maxIdleTime,"db-max-idle-time","15m","PostgreSQL max idle time")
+	flag.IntVar(&cfg.db.maxOpenConnection, "db-max-open-connection", 25, "PostgreSQL max open connection")
+	flag.IntVar(&cfg.db.maxIdleConnection, "db-max-idle-connection", 25, "PostgreSQL max idle connection")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max idle time")
 	flag.Parse()
 	//create a logger
-	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime )
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	//create the connection pool
 	db, err := openDB(cfg)
 	if err != nil {
@@ -60,16 +65,17 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 	//Create our new servermux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 	//create our Http server
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: app.routes(),// using the routes function from routes,go
-		IdleTimeout: time.Minute,
-		ReadTimeout: 10 * time.Second,
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(), // using the routes function from routes,go
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 	//start our server
@@ -78,8 +84,9 @@ func main() {
 	logger.Fatal(err)
 
 }
-//openDB() function returns a pointer *sql.DB connection pool
-func openDB(cfg config)(*sql.DB, error){
+
+// openDB() function returns a pointer *sql.DB connection pool
+func openDB(cfg config) (*sql.DB, error) {
 	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
 		return nil, err
@@ -92,7 +99,7 @@ func openDB(cfg config)(*sql.DB, error){
 	}
 	db.SetConnMaxIdleTime(duration)
 	//create a context with a 5 second timeout deadline
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	err = db.PingContext(ctx)
 	if err != nil {
